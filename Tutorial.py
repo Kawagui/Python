1d1df0c5ca70d0d0069f7fb8f18f8476457bd5b1
Kawagui <135202943+Kawagui@users.noreply.github.com>
2024-06-18 09:38:53 -0300

    Create Tutorial.py

---
+++ Tutorial.py
@@ -0,0 +1,12 @@
+#operadores aritméticos - +(adição), -(subtração), *(multiplicação), /(divisão), **(exponenciação), //(divisão interna), %(módulo)
+#operadores de atribuição - =(x = 1), +=(x += 1 => x = x + 1), -=(x -= 1 => x = x - 1), *=(x *= 1 => x = x * 1), /=(x /= 1 => x = x / 1), %=(x %= 1 => x = x % 1)
+#operadores de comparação - >(maior), <(menor), ==(igual), !=(diferente), >=(maior ou igual), <=(menor ou igual)
+#operadores lógicos - and, or, not
+#operadores de identidade(compara se os objetos são idênticos) - is, is not
+#operadores de associação(objeto está contido no conjunto?) - in, not in
+
+#tipos primitivos(não precisa declarar) - str(string), int, float, bool(boolean), complex
+
+#ação
+
+#seleção
